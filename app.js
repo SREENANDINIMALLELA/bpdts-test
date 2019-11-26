6d1b1bbffb87aa67a34e9cce154afd02cfddb76d
Nathan Maybrey <nathan.maybrey@ntlworld.com>
2019-11-26 17:56:58 +0000

    Refactored to use new route structure

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const usersRoutes = require('./routes/users/routes.js');
+
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
@@ -8,5 +10,7 @@ app.listen(3000, () => {
 app.get('/users', function(req, res) {
     res.send('List of users');
 })
+
+app.use('/', usersRoutes);
 
 module.exports = app;
---
+++ routes/users/functions.js
@@ -0,0 +1,9 @@
+function getUsers() {
+    // request('https://bpdts-test-app.herokuapp.com/city/London/users', {json: true}, (err, res, body) => {
+    //     if (err) { return console.log('An error occurred: ' + err); }
+
+    // });
+    console.log("Users Returned");
+}
+
+module.exports.getUsers = getUsers;
