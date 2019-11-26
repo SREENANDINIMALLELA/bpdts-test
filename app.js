37a67c20579b3f5cacad2faa206f4ef05ba4e92a
Nathan Maybrey <nathan.maybrey@ntlworld.com>
2019-11-26 22:12:31 +0000

    Created basic function to get distance to london using Bing maps API

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 
-app.get('/users', function(req, res) {
-    res.send('List of users');
-})
-
 app.use('/', usersRoutes);
 
 module.exports = app;
